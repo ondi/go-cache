3519e7aa40f93627ecf575a4e657da43950225ff
ondi <thy321@gmail.com>
2019-10-14 23:22:30 +0300

    minor fix

--- cache.go
+++ cache.go
@@ -207,8 +207,8 @@ func (self * Cache_t) Find(key interface{}) * Value_t {
 func (self * Cache_t) Remove(key interface{}) (ok bool) {
 	var it * Value_t
 	if it, ok = self.dict[key]; ok {
-		cut_list(it)
 		delete(self.dict, key)
+		cut_list(it)
 	}
 	return
 }
@@ -230,7 +230,7 @@ func (self * Cache_t) Size() int {
 }
 
 // takes linear time if sorted before
-func (self * Cache_t) InsertionSortFront(cmp Compare) {
+func (self * Cache_t) InsertionSortFront(cmp CmpLess) {
 	for it1 := self.Front().Next(); it1 != self.End(); it1 = it1.Next() {
 		for it2 := it1; it2.Prev() != self.End() && cmp.Less(it2, it2.Prev()); {
 			set_before(cut_list(it2), it2.Prev())
@@ -239,7 +239,7 @@ func (self * Cache_t) InsertionSortFront(cmp Compare) {
 }
 
 // takes linear time if sorted before
-func (self * Cache_t) InsertionSortBack(cmp Compare) {
+func (self * Cache_t) InsertionSortBack(cmp CmpLess) {
 	for it1 := self.Back().Prev(); it1 != self.End(); it1 = it1.Prev() {
 		for it2 := it1; it2.Next() != self.End() && cmp.Less(it2, it2.Next()); {
 			set_after(cut_list(it2), it2.Next())
@@ -247,18 +247,18 @@ func (self * Cache_t) InsertionSortBack(cmp Compare) {
 	}
 }
 
-type Compare interface {
+type CmpLess interface {
 	Less(a * Value_t, b * Value_t) bool
 }
 
 type reverse struct {
-	Compare
+	CmpLess
 }
 
 func (self * reverse) Less(a * Value_t, b * Value_t) bool {
 	return self.Less(b, a)
 }
 
-func Reverse(cmp Compare) Compare {
+func Reverse(cmp CmpLess) CmpLess {
 	return &reverse{cmp}
 }
