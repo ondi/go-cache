86bbcbb0553fc377596cfaf08276307861263be3
ondi <thy321@gmail.com>
2020-09-27 23:09:10 +0300

    minor fix

--- cache_test.go
+++ cache_test.go
@@ -14,11 +14,13 @@ func (*MyLess1_t) Less(a *Value_t, b *Value_t) bool {
 func Example_sort10() {
 	cc := New()
 	cc.CreateFront(1, func() interface{} { return 10 })
-	cc.WriteFront(5, func() interface{} { return 50 }, func(prev interface{}) interface{} { return prev })
-	cc.UpdateFront(1, func(interface{}) interface{} { return 100 })
+	cc.PushFront(5, func() interface{} { return 50 })
+	it, _ := cc.FindFront(1)
+	it.Update(100)
 	cc.CreateFront(7, func() interface{} { return 70 })
-	cc.WriteFront(3, func() interface{} { return 30 }, func(prev interface{}) interface{} { return prev })
-	cc.UpdateFront(5, func(interface{}) interface{} { return 500 })
+	cc.PushFront(3, func() interface{} { return 30 })
+	it, _ = cc.FindFront(5)
+	it.Update(500)
 
 	cc.InsertionSortFront(&MyLess1_t{})
 	for it := cc.Front(); it != cc.End(); it = it.Next() {
@@ -34,12 +36,13 @@ func Example_sort10() {
 func Example_sort20() {
 	cc := New()
 	cc.CreateFront(1, func() interface{} { return 10 })
-	cc.WriteFront(5, func() interface{} { return 50 }, func(prev interface{}) interface{} { return prev })
-	cc.UpdateFront(1, func(interface{}) interface{} { return 100 })
+	cc.PushFront(5, func() interface{} { return 50 })
+	it, _ := cc.FindFront(1)
+	it.Update(100)
 	cc.CreateFront(7, func() interface{} { return 70 })
-	cc.WriteFront(3, func() interface{} { return 30 }, func(prev interface{}) interface{} { return prev })
-	cc.UpdateFront(7, func(interface{}) interface{} { return 700 })
-
+	cc.PushFront(3, func() interface{} { return 30 })
+	it, _ = cc.FindFront(7)
+	it.Update(700)
 	cc.InsertionSortBack(&MyLess1_t{})
 	for it := cc.Front(); it != cc.End(); it = it.Next() {
 		fmt.Printf("%v %v\n", it.Key(), it.Value())
@@ -54,12 +57,13 @@ func Example_sort20() {
 func Example_swap10() {
 	cc := New()
 	cc.CreateFront(1, func() interface{} { return 10 })
-	cc.WriteFront(5, func() interface{} { return 50 }, func(prev interface{}) interface{} { return prev })
-	cc.UpdateFront(1, func(interface{}) interface{} { return 100 })
+	cc.PushFront(5, func() interface{} { return 50 })
+	it, _ := cc.FindFront(1)
+	it.Update(100)
 	cc.CreateFront(7, func() interface{} { return 70 })
-	cc.WriteFront(3, func() interface{} { return 30 }, func(prev interface{}) interface{} { return prev })
-	cc.UpdateFront(7, func(interface{}) interface{} { return 700 })
-
+	cc.PushFront(3, func() interface{} { return 30 })
+	it, _ = cc.FindFront(7)
+	it.Update(700)
 	it1, _ := cc.Find(1)
 	it2, _ := cc.Find(7)
 	Swap(it1, it2)
@@ -88,13 +92,14 @@ func Benchmark_cache10(b *testing.B) {
 func Benchmark_cache20(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		c2.WriteFront(i, func() interface{} { return i }, func(prev interface{}) interface{} { return prev })
+		c2.PushFront(i, func() interface{} { return i })
 	}
 }
 
 func Benchmark_cache30(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		c3.UpdateFront(i, func(interface{}) interface{} { return i })
+		it, _ := c3.FindFront(i)
+		it.Update(i)
 	}
 }
